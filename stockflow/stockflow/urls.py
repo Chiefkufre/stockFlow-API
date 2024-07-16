11ab50d228231cd6c174bb26392bebd68409dbfa
chiefkufre <samuelkufrewillie@gmail.com>
2024-07-16 15:01:10 +0100

    miscallenous

--- stockflow/stockflow/urls.py
+++ stockflow/stockflow/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/inventory/', include('inventory.urls')),
     path('api/supplier/', include('supplier.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
