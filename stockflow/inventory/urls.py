593a10f215170373dc830608df79fcb5e8235e92
chiefkufre <samuelkufrewillie@gmail.com>
2024-07-16 15:00:23 +0100

    feat: register new view in url ofinventory app

--- stockflow/inventory/urls.py
+++ stockflow/inventory/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ItemViewSet, SupplierItemViewSet
+from .views import ItemViewSet, ProductViewSet, SupplierItemViewSet
 
 router = DefaultRouter()
 router.register(r"items", ItemViewSet)
+router.register(r'products', ProductViewSet)
 
-""" I created this route for testing purposes but you can uncomment
+""" I registered this routes for testing purposes but you can uncomment
     if you intend to interest with the SupplierItem class
     All wrote will appear on swagger after you uncomment this
 """
 # router.register(r"items-supplier", SupplierItemViewSet)
+# router.register(r'imageurls', ImageUrlViewSet)
+
 
 urlpatterns = [
     path("", include(router.urls)),
